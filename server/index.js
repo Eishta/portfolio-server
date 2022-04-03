d426f8219bcff60763b3de0e5b718e025cdf2e80
Eishta <eishtamital@gmail.com>
2022-04-03 21:59:58 +0530

    updated  node version in package.json

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 var data = require('./data/userInfo.json');
 const blogs = require('./data/blogs.json');
 
 const axios = require('axios');
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 //***************** get Blogs *********************** 
 const getBlogsData = (data) => {
@@ -35,3 +39,8 @@ app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
